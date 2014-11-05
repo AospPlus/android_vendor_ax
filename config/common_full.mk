b241397325383bae1b826b8c98f99d721e031cb5
Tom Briden <tom@decompile.me.uk>
2014-11-05 10:59:29 +0000

    Start small for Lollipop

--- config/common_full.mk
+++ config/common_full.mk
@@ -1,18 +1,8 @@
 # Inherit common AX stuff
 $(call inherit-product, vendor/ax/config/common.mk)
 
-# Required AX packages
-PRODUCT_PACKAGES += \
-		Superuser \
-		su
-
 # Optional AX packages
 PRODUCT_PACKAGES += \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    VisualizationWallpapers \
-    PhaseBeam \
-		Email \
 		busybox
 
 # Openssh
@@ -24,21 +14,4 @@ PRODUCT_PACKAGES += \
     sshd_config \
     ssh-keygen \
 		rsync
-
-#F2FS
-PRODUCT_PACKAGES += \
-	mkfs.f2fs \
-	fsck.f2fs \
-	fibmap.f2fs \
-	f2fstat
-
-
-# T-Mobile theme engine
-#PRODUCT_PACKAGES += \
-#       ThemeManager \
-#       ThemeChooser \
-#       com.tmobile.themes
-
-#PRODUCT_COPY_FILES += \
-#       vendor/ax/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
 
