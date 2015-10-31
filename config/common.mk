c8cd445b6d40281396519f2fbc3a9cc2f8b56758
Tom Briden <tom@decompile.me.uk>
2015-10-31 14:06:54 +0000

    Enable launcher rotation

--- config/common.mk
+++ config/common.mk
@@ -11,7 +11,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
-    ro.com.android.dataroaming=false
+    ro.com.android.dataroaming=false \
+    log.tag.launcher_force_rotate=VERBOSE
 
 PRODUCT_COPY_FILES += \
     vendor/ax/prebuilt/system/media/bootanimation.zip:system/media/bootanimation.zip
